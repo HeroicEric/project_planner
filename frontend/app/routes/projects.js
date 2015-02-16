373d1b96a172bf4553e18b4b27c8ac9006d3e042
Eric Kelly <heroiceric@gmail.com>
2015-02-16 16:06:38 -0500

    Remove annoying redirect

--- frontend/app/routes/projects.js
+++ frontend/app/routes/projects.js
@@ -1,13 +1,7 @@
 import Ember from 'ember';
 
-var get = Ember.get;
-
 export default Ember.Route.extend({
   model: function() {
     return this.store.find('project');
-  },
-
-  redirect: function(projects) {
-    this.transitionTo('projects.show', get(projects, 'firstObject'));
   }
 });
